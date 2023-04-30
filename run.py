c295f312c88fb422f70da95c8e2f73cf7da490b1
Margarita Buyukli <maggypizza@hotmail.com>
2023-04-30 06:38:15 +0000

    Added results

--- run.py
+++ run.py
@@ -1,19 +1,13 @@
-'''
-imports
-'''
+# imports
 import random
 import hangman_picture
 from words import words
 
 
-'''
-Title
-'''
+# Title
 print("Welcome to Hangman Game")
 
-'''
-Rules of the game
-'''
+# Rules of the game
 print("Rules; Hangman is a simple word guessing game.")
 print("You have 6 chances to guess all the letters in the unknown word.")
 print("If you guess all the letters before your 6 chances are use, you win!")
@@ -22,23 +16,22 @@ print("Good luck!")
 print("Press P or p to start the game")
 
 
-'''
-Computer randomly selects the hangman word.
-'''
+# Computer randomly selects the hangman word.
 def select_word(words):
     return random.choice(words)
 
 
-'''
-The chosen word shows up as dashes while they have not been guessed.
-'''
-def print_hangman_word(hangman_word):
-    print(" _ " * len(hangman_word))
+# The chosen word shows up as dashes while they have not been guessed.
+def print_hangman_word(hangman_word, guessed_letters):
+    for letter in hangman_word:
+        if letter in guessed_letters:
+            print("{}".format(letter), end="")
+        else:
+            print(" _ ", end="")
+    print("\n")
 
 
-'''
-Check if guessed letter is in the work and to not allow user to guess anything except a single letter.
-'''
+# Check if guessed letter is in word & block double letters, numbers or signs
 def letter_input_correct(choice, hangman_word):
     if len(choice) > 1 or not choice.isalpha():
         print("You can only pick a single letter. Try again - \n")
@@ -50,13 +43,34 @@ def letter_input_correct(choice, hangman_word):
             return False
 
 
-'''
-Keeping track of guest's remaining guesses and which letters they have used up.
-'''
-remaining_attempts = 6
+def characters(word):
+    return"".join(set(word))
+
+# Keeping track of guest's remaining guesses & which letters they have used up
+
+
+chances_left = 6
 guessed_letters = ""
 hangman_word = select_word(words)
-choice = input("Pick a letter - \n")
-print(hangman_picture.get_hangman_picture(remaining_attempts))
-print_hangman_word(hangman_word)
+
+
 input_in_hangman_word = letter_input_correct(choice, hangman_word)
+if input_in_hangman_word:
+    if choice in guessed_letters:
+        print("You already guessed this letter. Pick another")
+    else:
+        print("You were right! That letter is part of the word.")
+else:
+    print("Unfortunately that letter is not part of the word. Try again.)
+    chances_left -= 1
+
+while chances_left > 0 and len(guessed_letters) <len(characters(hangman_word)):
+choice = input("Pick a letter - \n")
+print(hangman_picture.get_hangman_picture(chances_left))
+print("\n{} attempts_left\n".format(chances_left))
+print_hangman_word(hangman_word, guessed_letters)
+print("\n\nNumber of letters guessed -{}\n".format(len(guessed_letters)))
+if len(guessed_letters) == len(characters(hangman_word)):
+    print("Congratulations, you guessed the word right! You may live!\n)
+else:
+    print("Sorry, you lost!\n")
