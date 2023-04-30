61edbfa7dccaf5ec183e1f68a6b7e47ba9119e8d
Margarita Buyukli <maggypizza@hotmail.com>
2023-04-30 07:53:53 +0000

    Fixing name question bug

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ import random
 import hangman_picture
 from words import words
 
+
 def introduction():
     # Title
     print("Welcome to Hangman Game")
@@ -10,13 +11,13 @@ def introduction():
     # Rules of the game
     print("Rules; Hangman is a simple word guessing game.")
     print("You have 6 chances to guess all the letters in the unknown word.")
-    print("If you guess all the letters before your 6 chances are use, you win!")
+    print("If you guess all the letters, you win!")
     print("If you use up all your chances, you are hanged!")
     print("Good luck!")
-    print("What is yoour name? -\n")
-        if not name.isalpha():
-            print("Please enter your name using only letters -\n")
-        else:
+    name = ""
+    while not name.isalpha():
+        name = input("What is your name? (Use letters only) -\n")
+        if name.isalpha():
             print("Welcome {}! Press P or p to start the game".format(name))
 
     print("Press P or p to start the game")
@@ -41,7 +42,6 @@ def print_hangman_word(hangman_word, guessed_letters):
 def letter_input_correct(choice, hangman_word):
     if len(choice) > 1 or not choice.isalpha():
         print("You can only pick a single letter. Try again - \n")
-        sys.exit()
     else:
         if choice in hangman_word:
             return True
@@ -58,9 +58,9 @@ def characters(word):
 chances_left = 6
 guessed_letters = ""
 hangman_word = select_word(words)
+introduction()
 
-
-while chances_left > 0 and len(guessed_letters) <len(characters(hangman_word)):
+while chances_left > 0 and len(guessed_letters) < len(characters(hangman_word)):
     choice = input("Pick a letter - \n")
     input_in_hangman_word = letter_input_correct(choice, hangman_word)
     if input_in_hangman_word:
@@ -69,15 +69,15 @@ while chances_left > 0 and len(guessed_letters) <len(characters(hangman_word)):
         else:
             print("You were right! That letter is part of the word.")
     else:
-        print("Unfortunately that letter is not part of the word. Try again.)
+        print("Unfortunately that letter is not part of the word.")
         chances_left -= 1
 
     print(hangman_picture.get_hangman_picture(chances_left))
     print("\n{} attempts_left\n".format(chances_left))
     print_hangman_word(hangman_word, guessed_letters)
-    print("\n\nNumber of letters guessed -{}\n".format(len(guessed_letters)))
+    print("\n\nNumber of letters guessed correctly-{}\n".format(len(guessed_letters)))
 
- if len(guessed_letters) == len(characters(hangman_word)):
-    print("Congratulations, you guessed the word right! You may live!\n)
+if len(guessed_letters) == len(characters(hangman_word)):
+    print("Congratulations, you guessed the word right! You may live!\n")
 else:
     print("Sorry, you lost!\n")
